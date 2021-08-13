83377617e859b14f5344e5564b83ed3ce31304c7
Khalid <alhajr20@gmail.com>
2021-08-13 21:24:31 +0300

    Added api and desplay data

--- src/components/popular/Popular.js
+++ src/components/popular/Popular.js
@@ -1,41 +1,33 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+
+const IMG = 'https://image.tmdb.org/t/p/w500/';
+const API_KEY = '10f6488514e68084fc563f516e9d780a';
+const url = `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=${API_KEY}`;
 
 function Popular() {
+    const [movies, setMovies] = useState([]);
+
+    useEffect(() => {
+        fetch(url)
+            .then(data => data.json())
+            .then(res => setMovies(res.results));
+    }, []);
+
     return (
         <section className="popular">
             <div className="container">
                 <h2 className="popular__title">Popular</h2>
                 <div className="popular__wrapper">
-                    <div className="popular__item">
-                        <img src="https://img03.rl0.ru/afisha/e336x480q65i/s2.afisha.ru/mediastorage/9e/d6/3181da71565b4abd80f79ddbd69e.jpg" alt="popular image" />
-                        <div className="popular__item-more">
-                            <a href="#"><ion-icon name="information-circle-outline"></ion-icon> More Info</a>
-                        </div>
-                    </div>
-                    <div className="popular__item">
-                        <img src="https://img09.rl0.ru/afisha/e336x480q65i/s.afisha.ru/mediastorage/d6/bf/d8be376111664ee1925c5bd6bfd6.jpg" />
-                        <div className="popular__item-more">
-                            <a href="#"><ion-icon name="information-circle-outline"></ion-icon> More Info</a>
-                        </div>
-                    </div>
-                    <div className="popular__item">
-                        <img src="https://img09.rl0.ru/afisha/e336x480q65i/s1.afisha.ru/mediastorage/25/1b/140393315d8844179f2692231b25.jpg" alt="popular image" />
-                        <div className="popular__item-more">
-                            <a href="#"><ion-icon name="information-circle-outline"></ion-icon> More Info</a>
-                        </div>
-                    </div>
-                    <div className="popular__item">
-                        <img src="https://img02.rl0.ru/afisha/e336x480q65i/s5.afisha.ru/mediastorage/16/3e/6c5330e03e734b9d984d74073e16.jpg" alt="popular image" />
-                        <div className="popular__item-more">
-                            <a href="#"><ion-icon name="information-circle-outline"></ion-icon> More Info</a>
-                        </div>
-                    </div>
-                    <div className="popular__item">
-                        <img src="https://img08.rl0.ru/afisha/e336x480q65i/s.afisha.ru/mediastorage/11/3f/acc43d9c9dd24e28a4827d7c3f11.jpg" alt="popular image" />
-                        <div className="popular__item-more">
-                            <a href="#"><ion-icon name="information-circle-outline"></ion-icon> More Info</a>
-                        </div>
-                    </div>
+                    {
+                        movies.map((item, i) => (
+                            <div key={i} className="popular__item">
+                                <img src={IMG+item.poster_path} alt="popular image" />
+                                <div className="popular__item-more">
+                                    <a href="#"><ion-icon name="information-circle-outline"></ion-icon> More Info</a>
+                                </div>
+                            </div>
+                        ))
+                    }
                 </div>
             </div>
         </section>
