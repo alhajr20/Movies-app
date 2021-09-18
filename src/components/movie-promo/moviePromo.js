c6af485f41131631101e6deba8c4f539e32f756d
Khalid <alhajr20@gmail.com>
2021-09-18 23:43:25 +0300

    Fixed bags

--- src/components/movie-promo/moviePromo.js
+++ src/components/movie-promo/moviePromo.js
@@ -2,21 +2,7 @@ import React, {useState, useEffect} from 'react';
 
 const IMG = 'https://image.tmdb.org/t/p/w500/';
 
-function MoviePromo() {
-    const [movie, setMovie] = useState([]);
-
-    useEffect(() => {
-        async function fetchData() {
-            const request = await fetch(`https://api.themoviedb.org/3/movie/${window.location.href.split('movie/')[1]}?api_key=10f6488514e68084fc563f516e9d780a`);
-            const data = await request.json();
-
-            setMovie(data);
-            
-            return data;
-        }
-
-        fetchData();
-    }, []);
+function MoviePromo({movie}) {
 
     return (
         <section className="movie-promo">
--- src/pages/Movie.js
+++ src/pages/Movie.js
@@ -1,11 +1,28 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
 
 import MoviePromo from '../components/movie-promo/moviePromo';
 
+const IMG = 'https://image.tmdb.org/t/p/w500/';
+
 function Movie() {
+    const [movie, setMovie] = useState([]);
+
+    useEffect(() => {
+        async function fetchData() {
+            const request = await fetch(`https://api.themoviedb.org/3/movie/${window.location.href.split('movie/')[1]}?api_key=10f6488514e68084fc563f516e9d780a`);
+            const data = await request.json();
+
+            setMovie(data);
+            
+            return data;
+        }
+
+        fetchData();
+    }, []);
+
     return (
         <Fragment>
-            <MoviePromo/>
+            <MoviePromo movie={movie} />
         </Fragment>
     );
 }
